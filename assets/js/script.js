e285bae16aa93939b09f5eb75a5d73216b1b6ed3
Jen Truong <jennifertruong09@gmail.com>
2023-04-19 20:12:18 -0400

    added numbers to board

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,10 @@ function setGame() {
         for (let c=0; c<9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tileStart")
+            };
             tile.addEventListener("click", selectTile)
             tile.classList.add("tile");
             document.getElementById("board").append(tile);
@@ -64,6 +68,10 @@ function selectNumber() {
 
 function selectTile() {
     if(numSelected) {
+        if (this.innerText != "") {
+            return
+        }
         this.innerText =numSelected.id
     }
+
 }
